498dd12a669dcc4f8c478098223a84c2b100caed
DIIA <opensource@thedigital.gov.ua>
2024-08-05 13:28:07 +0000

    Releasing genproto

--- src/command/tsCommandBuilder.ts
+++ src/command/tsCommandBuilder.ts
@@ -1,15 +1,49 @@
 import { execSync } from 'node:child_process'
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 
 import { CommandBuilder, Platform } from './index'
 
 export default class TsCommandBuilder extends CommandBuilder {
+    // By default ts plugin should be linked to .bin dir but in case if it doesn't
+    // try to look it up in "standard" locations
+    private tsPluginLocations = [
+        './node_modules/.bin/protoc-gen-ts_proto',
+        './node_modules/ts-proto/protoc-gen-ts_proto',
+        './node_modules/@diia-inhouse/genproto/node_modules/ts-proto/protoc-gen-ts_proto',
+    ]
+
+    private async isFileExists(file: string): Promise<boolean> {
+        try {
+            await fs.access(file, fs.constants.F_OK)
+        } catch {
+            return false
+        }
+
+        return true
+    }
+
+    async getProtoTsPluginLocation(): Promise<string | undefined> {
+        const pluginLocations = []
+
+        for (const location of this.tsPluginLocations) {
+            try {
+                await fs.realpath(location)
+            } catch {
+                continue
+            }
+
+            pluginLocations.push(location)
+        }
+
+        return pluginLocations.pop()
+    }
+
     async protocCommand(): Promise<string[]> {
         let projectPlatform: Platform
 
-        if (fs.existsSync('package.json') && !fs.existsSync('build.gradle')) {
+        if ((await this.isFileExists('package.json')) && !(await this.isFileExists('build.gradle'))) {
             projectPlatform = Platform.ts
-        } else if (fs.existsSync('build.gradle')) {
+        } else if (await this.isFileExists('build.gradle')) {
             projectPlatform = Platform.java
         } else {
             throw new Error('Unable to identify platform type no package.json or build.gradle found')
@@ -35,10 +69,16 @@ export default class TsCommandBuilder extends CommandBuilder {
             }
         }
 
+        const pluginLocation = this.getProtoTsPluginLocation()
+
+        if (!pluginLocation) {
+            throw new Error("Couldn't locate plugin in node modules. Are you sure that ts-proto is installed?")
+        }
+
         const command = [
             'protoc',
             '--experimental_allow_proto3_optional',
-            '--plugin=$(npm root)/.bin/protoc-gen-ts_proto',
+            `--plugin=${pluginLocation}`,
             '--ts_proto_opt=useSnakeTypeName=false',
             '--ts_proto_opt=unrecognizedEnum=false',
             '--ts_proto_opt=stringEnums=true',
